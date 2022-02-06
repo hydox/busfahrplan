bd224befcc439294ca609f3935e827f88089acc5
Christof03 <74654405+Christof03@users.noreply.github.com>
2022-02-06 15:28:14 +0100

    check if array has been modified

--- js/main.js
+++ js/main.js
@@ -50,10 +50,10 @@ async function fetchBusData(bool){
 
    
     if(bool){
-         url ="https://efa.sta.bz.it/web/XML_DM_REQUEST?sessionID=0&requestID=0&command=&useRealtime=1&coordOutputFormat=WGS84%5BDD.ddddd%5D&locationServerActive=1&mode=direct&useAllStops=1&depType=STOPEVENTS&includeCompleteStopSeq=1&calcOneDirection=1&dmLineSelectionAll=1&limit=10&itdDate="+year+""+ month+""+day+"&itdTime="+hour+""+minute+"&itdTripDateTimeDepArr=dep&ptOptionsActive=0&imparedOptionsActive=0&changeSpeed=normal&lineRestriction=400&maxChanges=9&routeType=leasttime&includedMeans=checkbox&inclMOT_BUS=true&inclMOT_ZUG=true&inclMOT_8=true&name_dm=66001143&type_dm=stop&language=de&outputFormat=JSON&outputEncoding=UTF-8"
+         url ="https://efa.sta.bz.it/web/XML_DM_REQUEST?sessionID=0&requestID=0&command=&useRealtime=1&coordOutputFormat=WGS84%5BDD.ddddd%5D&locationServerActive=1&mode=direct&useAllStops=1&depType=STOPEVENTS&includeCompleteStopSeq=1&calcOneDirection=1&dmLineSelectionAll=1&limit=15&itdDate="+year+""+ month+""+day+"&itdTime="+hour+""+minute+"&itdTripDateTimeDepArr=dep&ptOptionsActive=0&imparedOptionsActive=0&changeSpeed=normal&lineRestriction=400&maxChanges=9&routeType=leasttime&includedMeans=checkbox&inclMOT_BUS=true&inclMOT_ZUG=true&inclMOT_8=true&name_dm=66001143&type_dm=stop&language=de&outputFormat=JSON&outputEncoding=UTF-8"
 
     }else{
-         url= "https://efa.sta.bz.it/web/XML_DM_REQUEST?sessionID=0&requestID=0&command=&useRealtime=1&coordOutputFormat=WGS84%5BDD.ddddd%5D&locationServerActive=1&mode=direct&useAllStops=1&depType=STOPEVENTS&includeCompleteStopSeq=1&calcOneDirection=1&dmLineSelectionAll=1&limit=5&itdDate="+year+""+ month+""+day+"&itdTime="+hour+""+minute+"&itdTripDateTimeDepArr=dep&ptOptionsActive=0&imparedOptionsActive=0&changeSpeed=normal&lineRestriction=400&maxChanges=9&routeType=leasttime&includedMeans=checkbox&inclMOT_ZUG=true&name_dm=66000998&type_dm=stop&language=de&outputFormat=JSON&outputEncoding=UTF-8"
+         url= "https://efa.sta.bz.it/web/XML_DM_REQUEST?sessionID=0&requestID=0&command=&useRealtime=1&coordOutputFormat=WGS84%5BDD.ddddd%5D&locationServerActive=1&mode=direct&useAllStops=1&depType=STOPEVENTS&includeCompleteStopSeq=1&calcOneDirection=1&dmLineSelectionAll=1&limit=3&itdDate="+year+""+ month+""+day+"&itdTime="+hour+""+minute+"&itdTripDateTimeDepArr=dep&ptOptionsActive=0&imparedOptionsActive=0&changeSpeed=normal&lineRestriction=400&maxChanges=9&routeType=leasttime&includedMeans=checkbox&inclMOT_ZUG=true&name_dm=66000998&type_dm=stop&language=de&outputFormat=JSON&outputEncoding=UTF-8"
 
     }
 
@@ -115,17 +115,21 @@ function displayWeather(weatherdata){
 }
 displayWeather.counter=0;
 
+busandtrainhistory=["test"];
 
 async function getBusData(){
     bus = await fetchBusData(true);
 
     train = await fetchBusData(false);
 
+    console.log(bus);
+    console.log(train);
+
     busandtrain=[];
+    
 
     
   
-
    for(x of bus){
     
         if(x.dateTime.hour.length == 1) {
@@ -135,7 +139,7 @@ async function getBusData(){
             x.dateTime.minute = '0'+x.dateTime.minute;
         }
        
-       busandtrain.push([x.servingLine.direction,x.servingLine.number,x.dateTime.hour+":"+x.dateTime.minute, x.countdown,x.onwardStopSeq[0].ref.arrDelay]);
+       busandtrain.push([x.servingLine.direction,x.servingLine.number,x.dateTime.hour+":"+x.dateTime.minute, x.countdown,x.onwardStopSeq[0].ref.arrDelay,"bus"]);
    }
 
    for(x of train){
@@ -146,7 +150,7 @@ async function getBusData(){
         x.dateTime.minute = '0'+x.dateTime.minute;
     }
 
-    busandtrain.push([x.servingLine.direction,x.servingLine.number.slice(0,8),x.dateTime.hour+":"+x.dateTime.minute, x.countdown,x.servingLine.delay]);  
+    busandtrain.push([x.servingLine.direction,x.servingLine.number.slice(0,8),x.dateTime.hour+":"+x.dateTime.minute, x.countdown,x.servingLine.delay,"train"]);  
    }
 
    busandtrain.sort(sortFunction);
@@ -168,12 +172,25 @@ function sortFunction(a, b) {
 }
 
    console.log(busandtrain);
+   console.log(busandtrainhistory);
+
+   if(busandtrain[0][0]!=busandtrainhistory[0][0]){
+        console.log("falsch");//falls array verändert wird 
+   }
+
+   busandtrainhistory=[];
+
+   for(x of busandtrain){
+       busandtrainhistory.push(x);
+   }
+
+   
   
 };
 
 arr=["Brixen", "Sterzing", "Bozen", "Bruneck", "Klausen", "Brenner"];
 
-setInterval(start,10000);
+
 
 async function start(){
     for(x of arr){
@@ -181,4 +198,8 @@ async function start(){
     }
     getBusData();
 }
+
+start();
+
+setInterval(start,60000);
 
